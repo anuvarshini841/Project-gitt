88f9f497d8375f8c7b12c26f547bf5294c587372
anuvarshini841 <anuvarshini301@gmail.com>
2023-03-29 22:15:20 +0530

    Test Ongoing

--- src/test/java/org/samplee/SampleeTest.java
+++ src/test/java/org/samplee/SampleeTest.java
@@ -1,5 +1,23 @@
 package org.samplee;
 
 public class SampleeTest {
+	
+	public void personalname() {
+		System.out.println("Harsha");
+	}
+	
+	public void personalId() {
+		System.out.println("123");
+	}
+	
+	public static void main(String[] args) {
+		
+		SampleeTest s = new SampleeTest();
+		s.personalId();
+		s.personalname();
+		
+		
+	}
+	
 
 }
